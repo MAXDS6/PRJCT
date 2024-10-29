459ef43dc2b96f03d65fc16fab6c904324c8e6de
Egigogne <egigogne@alumnos.uai.cl>
2024-10-28 21:40:17 -0300

    Update page.js
    Se modifico page.js para que tuviesen imagenes

--- src/app/page.js
+++ src/app/page.js
@@ -43,6 +43,19 @@ const textContent = {
     }
 };
 
+// Añade un objeto para las URLs de imágenes de los productos
+const productImages = {
+    1: "https://api.autoplanet.cl/medias/sys_master/images/h03/hbb/9689390415902/19_Sistema-de-Escape/19-Sistema-de-Escape.png",
+    2: "https://api.autoplanet.cl/medias/sys_master/images/h00/ha0/9627906605086/1088886_1-1682041641/1088886-1-1682041641.webp",
+    3: "https://api.autoplanet.cl/medias/sys_master/images/h30/h37/9742911635486/drp300Wx300H_1145915_1-1718052059/drp300Wx300H-1145915-1-1718052059.webp",
+    4: "https://api.autoplanet.cl/medias/sys_master/images/h14/h3a/9637833244702/drp515Wx515H_123007_1-1682041641/drp515Wx515H-123007-1-1682041641.webp",
+    5: "https://api.autoplanet.cl/medias/sys_master/images/hec/hf5/9657979699230/drp515Wx515H_1104740_1-1682041641/drp515Wx515H-1104740-1-1682041641.webp",
+    6: "https://api.autoplanet.cl/medias/sys_master/images/h3a/h02/9659757068318/1129025_1-1682041641/1129025-1-1682041641.webp",
+    7: "https://api.autoplanet.cl/medias/sys_master/images/h1c/h25/9671835648030/1089846_1-1686325795/1089846-1-1686325795.webp",
+    8: "https://api.autoplanet.cl/medias/sys_master/images/h41/hcd/9658013843486/drp515Wx515H_1108485_1-1682041641/drp515Wx515H-1108485-1-1682041641.webp",
+    9: "https://api.autoplanet.cl/medias/sys_master/images/h52/h09/9636977573918/121733_1-1682041641/121733-1-1682041641.webp"
+};
+
 export default function Home() {
     const [cart, setCart] = useState({});
     const [total, setTotal] = useState(0);
@@ -106,7 +119,7 @@ export default function Home() {
                 {Object.keys(textContent[language].products).map((id) => (
                     <div key={id} className="product">
                         <Image
-                            src={`https://source.unsplash.com/120x120/?product,${id}`}
+                            src={productImages[id]} // Usa la URL específica de cada producto
                             alt={textContent[language].products[id]}
                             width={120}
                             height={120}
